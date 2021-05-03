49e4cd5b79de8472e4c293a9e075037ec37237d5
Hiroaki Takisawa <takisawa@hit-start.jp>
2021-05-03 10:11:24 +0900

    add 2.1.md

--- 2/2.1.rb
+++ 2/2.1.rb
@@ -11,13 +11,14 @@ def show(x)
   end
 end
 
-puts "n,log2(n),sqrt(n),n*log2(n),n^2,n^3,2^n,n!"
+puts "n,log2(n),sqrt(n),n*log2(n),n*sqrt(n),n^2,n^3,2^n,n!"
 [1,2,3,5,10,20,50,100,1000,10_000,100_000,1_000_000,10_000_000,100_000_000,1000_000_000].each.with_index do |n, i|
   puts [
     n,
     Math.log(n, 2),
     Math.sqrt(n),
     n * Math.log(n, 2),
+    n * Math.sqrt(n),
     n ** 2,
     n ** 3,
     (i < 10) ? 2 ** n : '-',
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## study-algo-ds
 
-回答URL: `https://github.com/drken1215/book_algorithm_solution`
+[回答URL](https://github.com/drken1215/book_algorithm_solution)
