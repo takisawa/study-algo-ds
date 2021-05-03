0edc7c9fb93b899960f0ef81d68ebbc86bb07bb8
Hiroaki Takisawa <takisawa@hit-start.jp>
2021-05-03 09:55:49 +0900

    add 2.1.*

---
+++ 2/2.1.rb
@@ -0,0 +1,26 @@
+
+def show(x)
+  if x == '-'
+    return '-'
+  end
+
+  if x > 1000_000_000_000_000_000_000
+    '-'
+  else
+    x.to_i
+  end
+end
+
+puts "n,log2(n),sqrt(n),n*log2(n),n^2,n^3,2^n,n!"
+[1,2,3,5,10,20,50,100,1000,10_000,100_000,1_000_000,10_000_000,100_000_000,1000_000_000].each.with_index do |n, i|
+  puts [
+    n,
+    Math.log(n, 2),
+    Math.sqrt(n),
+    n * Math.log(n, 2),
+    n ** 2,
+    n ** 3,
+    (i < 10) ? 2 ** n : '-',
+    (i < 10) ? (1..n).inject(1,:*) : '-',
+  ].map {|x| show(x) }.join(',')
+end
